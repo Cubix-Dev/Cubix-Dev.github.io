3ee1f2cb5ad2a1d051abbafb210c89966d0c4ce5
Kaylerr <71836631+Kaylerr@users.noreply.github.com>
2021-12-29 16:04:18 +0000

    Update SNextDevTools.js

--- source code/SNextDevTools.js
+++ source code/SNextDevTools.js
@@ -47,14 +47,14 @@ class DevTools {
         
     }
  function fetchUiD() {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    var charactersLength = characters.length;
+    var result           = ''
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    var charactersLength = characters.length
     for ( var i = 0; i < 18; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
+ charactersLength))
    }
-   return result;
+   return result
 }
 
 
