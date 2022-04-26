9d86dc7555e475a3a2de37f4665ef08704d6d9ad
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-04-26 02:46:36 +0000

    Fix broken copy-paste methods

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function myFunction() {
-  var copyText = document.getElementById("myInput");
+  var copyText = document.getElementById("SNC-Link");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied Text";
+}
+
+function myFunction() {
+  var copyText = document.getElementById("Kiwi-Link");
   copyText.select();
   copyText.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(copyText.value);
