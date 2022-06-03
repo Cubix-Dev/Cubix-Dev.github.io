434824a65d43be736f56f45fd2f07d549e496a6b
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-06-03 22:10:51 +0000

    Major Update

--- index.js
+++ index.js
@@ -8,17 +8,22 @@ function myFunction() {
   tooltip.innerHTML = "Copied Text";
 }
 
-function myFunction() {
+function myFunction2() {
   var copyText = document.getElementById("Kiwi-Link");
   copyText.select();
   copyText.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(copyText.value);
   
-  var tooltip = document.getElementById("myTooltip");
+  var tooltip = document.getElementById("myTooltip2");
   tooltip.innerHTML = "Copied Text";
 }
 
 function outFunc() {
   var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy to clipboard";
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip2");
   tooltip.innerHTML = "Copy to clipboard";
 }
