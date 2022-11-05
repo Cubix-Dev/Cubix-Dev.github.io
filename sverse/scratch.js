1d34be63b508998a2c6a40488bd7217d21472efb
Kaylerr <71836631+Kaylerr@users.noreply.github.com>
2022-11-05 19:21:26 +0000

    Update and rename UserManager.js to scratch.js

---
+++ sverse/scratch.js
@@ -0,0 +1,76 @@
+//Welcome guys! The original code here is made by Kaylerr. Thanks for checking this out, and make any changes necessary.
+
+class SVerseSDK {
+    constructor() {
+    }
+    
+    getInfo() {
+        return {
+            "id": "SVerse SDK",
+            "name": "SVerse",
+            "blocks": [
+                        {
+                            "opcode": "authenticateUser",
+                            "blockType": "reporter",
+                            "text": "Log In With SVerse with Username [username] and Password [password],
+                            "arguments": {
+                                "password": {
+                                    "type": "string",
+                                    "defaultValue": "RelicStar"
+                                },
+                                 "password": {
+                                    "type": "string",
+                                    "defaultValue": "Is Awesome"
+                                },
+                            }
+                        },
+                ],
+        };
+    }
+    
+    authenticateUser({username, password}) {
+const url = new URL('https://cube-enix.github.io/sverse/Server.html?username=Kaylerr&password=isCool');
+const params1 = new URLSearchParams(url.search);
+        
+    //Next, let's check if we have a "username" in our url.
+    if(params1.has('username')) {
+
+        //Seems like we do! If we check for the "password" in our url?
+        if(params1.has('password')) {
+
+            //okay, let's try to use them here.
+            
+            const user = params1.get('username');
+            const pass = params1.get('password');
+
+            //add authentication shit here
+            return "Authenticated User!"
+
+        } else {
+            //Nope! Let's return an error to the console.
+        return "Failed to find a Password in the URL."
+        } 
+
+    } else {
+        //Nope! Let's return an error to the console.
+        return "Failed to find a Username in the URL."
+    }
+    
+    jsonExtract({name,data}) {
+        var parsed = JSON.parse(data)
+        if (name in parsed) {
+            var out = parsed[name]
+            var t = typeof(out)
+            if (t == "string" || t == "number")
+                return out
+            if (t == "boolean")
+                return t ? 1 : 0
+            return JSON.stringify(out)
+        }
+        else {
+            return ""
+        }
+    }
+}
+
+Scratch.extensions.register(new ScratchFetch())
