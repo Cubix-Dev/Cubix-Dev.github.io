cd5f10a78a0cf0ad704f776eb634be9c5fb3629b
Kaylerr <itscookiebunny49@gmail.com>
2022-11-05 19:58:10 +0000

    scratch.extensions.what??!

--- sverse/scratch.js
+++ sverse/scratch.js
@@ -56,22 +56,6 @@ class SVerseSDK {
         return "Failed to find a Username in the URL."
     }
 }
-    
-    jsonExtract({name,data}) {
-        var parsed = JSON.parse(data)
-        if (name in parsed) {
-            var out = parsed[name]
-            var t = typeof(out)
-            if (t == "string" || t == "number")
-                return out
-            if (t == "boolean")
-                return t ? 1 : 0
-            return JSON.stringify(out)
-        }
-        else {
-            return ""
-        }
-    }
 }
 
 Scratch.extensions.register(new SVerseSDK())
