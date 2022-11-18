afd087f8799fdbda946b0650d97ca40c2886c36a
Kaylerr <itscookiebunny49@gmail.com>
2022-11-18 23:42:04 +0000

    falling asleep while trying to secure sverse

--- sverse/index.js
+++ sverse/index.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAUw_AFGzf6LfvkfCMnswH_8Z06UhLQ2jE",
-  authDomain: "snext-sverse.firebaseapp.com",
+  apiKey: fetch("https://raw.githubusercontent.com/Cube-Enix/private-stuff/main/apiKey?token=GHSAT0AAAAAABY324F4TN3VHMVXD322PKNSY3YC6DQ").then(apiKey => apiKey.text()),
+  authDomain: fetch("https://raw.githubusercontent.com/Cube-Enix/private-stuff/main/authDomain?token=GHSAT0AAAAAABY324F4ADG7AC3DGRYEEIL6Y3YC6JQ").then(authDomain => authDomain.text()),
   projectId: "snext-sverse",
-  storageBucket: "snext-sverse.appspot.com",
-  messagingSenderId: "1091633315944",
-  appId: "1:1091633315944:web:4dab13b7d41145e802bbdd",
-  measurementId: "G-G2N2XNZPR7"
+  storageBucket: fetch("https://raw.githubusercontent.com/Cube-Enix/private-stuff/main/storageBucket?token=GHSAT0AAAAAABY324F43UJCWUN5HOGBGU6IY3YC6UQ").then(storageBucket => storageBucket.text()),
+  messagingSenderId: fetch("https://raw.githubusercontent.com/Cube-Enix/private-stuff/main/messagingSenderID?token=GHSAT0AAAAAABY324F5TIAPKQFEZRBCKZEOY3YC6OQ").then(messagingSenderId => messagingSenderId.text()),
+  appId: fetch("https://raw.githubusercontent.com/Cube-Enix/private-stuff/main/appID?token=GHSAT0AAAAAABY324F5QWZW46JVYF2USSPUY3YC6GA").then(appId => appId.text()),
+  measurementId: fetch("https://raw.githubusercontent.com/Cube-Enix/private-stuff/main/measurementiD?token=GHSAT0AAAAAABY324F4PXPS2GRMWOIWBDDWY3YC6MA").then(measurementId => measurementId.text()),
 };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
@@ -60,7 +60,7 @@ const firebaseConfig = {
       database_ref.child('users/' + user.uid).set(user_data)
   
       // DOne
-      alert('User Created!!')
+      window.location.href = "https://cube-enix.github.io/sverse/pages/AuthenticationSuccess.html";
     })
     .catch(function(error) {
       // Firebase will use this to alert of its errors
@@ -108,7 +108,7 @@ const firebaseConfig = {
       database_ref.child('users/' + user.uid).update(user_data)
   
       // DOne
-      alert('User Logged In!!')
+      window.location.href = "https://cube-enix.github.io/sverse/pages/AuthenticationSuccess.html"
   
     })
     .catch(function(error) {
