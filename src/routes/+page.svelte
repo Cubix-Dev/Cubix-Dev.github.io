244d8e4723f2b450e964b0e7dfea8045690b955a
Ship <silverteamroblox@gmail.com>
2023-11-20 18:52:32 -0500

    kayler did a thing :pray:
    Co-authored-by: Kaylerr <skyewontmiss@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
-<script>
+<script lang="ts">
   import { CashStack } from "svelte-bootstrap-icons";
 	import logo from '$lib/assets/Cube.svg';
-  import console from '$lib/assets/ImgConsole.png'
-  import Infinite from '$lib/assets/infinite.svg'
+  import console from '$lib/assets/ImgConsole.png';
+  import Infinite from '$lib/assets/infinite.svg';
+  
+  function openURL(uri: string) {
+        window.open(uri);
+      }
 </script>
 
 <main>
@@ -25,7 +29,7 @@
          <h2 class="header-subtitle"> Infinite Ways To Play </h2>
       </div>
    </header>
-
+<!--
    <div class="inline-box-delimited justify">
     <div class="vertin-box bg-dark text-white">
       <img class="vertin-box-img" src={logo} alt="Cubix Logo">
@@ -42,7 +46,29 @@
       <h1 class="vertin-box-title">Cubix Blog</h1>
       <p>Read the latest new regarding Cubix and the Cubix Infinite.</p>
      </div>
-   </div>
+    -->
+
+     <!--About Cubix-->
+     <button class="vertin-box bg-dark text-white" on:click={() => openURL('https://google.com')}>
+      <img class="vertin-box-img" src={logo} alt="Cubix Logo">
+      <h1 class="vertin-box-title">About Cubix</h1>
+      <p>Learn more about the Cubix organzation, what we do and how we do it.</p>
+    </button>
+
+         <!--Cubix Infinite Page-->
+    <button class="vertin-box bg-dark text-white" on:click={() => openURL('https://google.com')}>
+      <img class="vertin-box-img" src={Infinite} alt="Cubix Infinite">
+      <h1 class="vertin-box-title">About The Cubix Infinite</h1>
+      <p>Learn more about the Cubix Infinite, exploring specs, benchmark test and more.</p>
+    </button>
+
+         <!--Blog-->
+    <button class="vertin-box bg-dark text-white" on:click={() => openURL('https://google.com')}>
+      <img class="vertin-box-img" src={Infinite} alt="Cubix Infinite">
+      <h1 class="vertin-box-title">Cubix Blog</h1>
+      <p>Read the latest new regarding Cubix and the Cubix Infinite.</p>
+    </button>
+    
    <br><br>
     
     <!-- <div class="header-spacer">
@@ -83,7 +109,9 @@
         <h2 class="header-title margin-top">Indie Powered</h2>
         <p class="text-white">The Infinite doesn't just give you big names, it supports the little guys too. With an extensive Indie dev library, you can enjoy the creations of developers from communities far and wide like <strong>TheUpdator</strong> and <strong> CodeKirby </strong>. Plus the system's Developer API is extremely developer friendly. You can become a developer on the Cubix Infinite with just a few clicks!</p>
       </div>
-      <a class="button btn-pink" href="#">Register</a>
+      <button class="small-anim pink" on:click={() => openURL('https://google.com')}>
+        Developer Portal
+       </button>
     </div>
     <br><br>
     <div class="flex-box-text-center pad bg-black">
@@ -100,7 +128,10 @@
        <h2 class="header-title margin-top"><CashStack width={24} height={24}/> Join The Early Supporters <CashStack width={24} height={24}/></h2>
        <p class="text-white">Cubix needs your help! You can become an early supporter for the Cubix Infinite by making a donation of any amount to Cubix Organization via our kickstarter! Your donation goes towards the continued development of the Cubix Infinite and helps solidify its position on market.</p>
      </div>
-     <a class="button btn-green" href="#">Support Us</a>
+     
+     <button class="small-anim pink" on:click={() => openURL('https://google.com')}>
+      Support Us
+     </button>
     </div>
     <br>
   </section>
@@ -118,4 +149,5 @@
       </div>
     </div>
   </div> -->
+
 </main>
